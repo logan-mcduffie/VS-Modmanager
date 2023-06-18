a8e1db7fcdbdb022128cc69582afb07047c27f10
Logan McDuffie <loganmcduffie@gmail.com>
2023-06-18 17:31:45 -0600

    lots of adjustments
    refactored renderer.js code to be far more readable (thanks chatgpt)
    integrated a file system into main.js that initializes the folder to hold app info
    added page to browse modpacks for the sake of testing

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const { app, BrowserWindow } = require('electron')
+const fs = require('fs');
+const path = require('path');
+const appDataPath = process.env.APPDATA || (process.platform === 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share");
+const modpalFolderPath = path.join(appDataPath, 'Modpal');
 require('@electron/remote/main').initialize()
 
 const createWindow = () => {
   const win = new BrowserWindow({
     backgroundColor: '#2e2c29',
-    width: 1024,
-    height: 768,
+    width: 1120,
+    height: 780,
+    minWidth: 1120,
+    minHeight: 780,
     frame: false,
     webPreferences: {
       enableRemoteModule: true,
@@ -13,6 +19,10 @@ const createWindow = () => {
       contextIsolation: false,
     }
   })
+
+  fs.mkdir(modpalFolderPath, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
 
   win.loadFile('index.html')
 
--- src/renderer.js
+++ src/renderer.js
@@ -1,127 +1,84 @@
-const { BrowserWindow } = require('@electron/remote')
+// Import required modules
+const { BrowserWindow } = require('@electron/remote');
+
+// Define UI elements
 const myModpacksButton = document.getElementById('my-modpacks-button');
 const browseModpacksButton = document.getElementById('browse-modpacks-button');
+const modal = document.getElementById("myModal");
+const createModpackButton = document.getElementById("create-modpack-button");
+const closeButton = document.getElementsByClassName("close")[0];
+const form = document.getElementById('modpackForm');
+const modpackLogoButton = document.getElementById('modpackLogoButton');
+const modpackLogoInput = document.getElementById('modpackLogo');
 
+// Add 'active' class to 'myModpacksButton' by default
 myModpacksButton.classList.add('active');
 
-myModpacksButton.addEventListener('click', function() {
-    if (!myModpacksButton.classList.contains('active')) {
-      document.getElementById('my-modpacks').style.display = 'block';
-      document.getElementById('browse-modpacks').style.display = 'none';
-      myModpacksButton.classList.add('active');
-      browseModpacksButton.classList.remove('active');
+// Event listeners for switching views
+myModpacksButton.addEventListener('click', () => switchView(myModpacksButton, browseModpacksButton, 'my-modpacks', 'browse-modpacks'));
+browseModpacksButton.addEventListener('click', () => switchView(browseModpacksButton, myModpacksButton, 'browse-modpacks', 'my-modpacks'));
+
+// Event listeners for window controls
+document.getElementById('minimize').addEventListener('click', () => BrowserWindow.getFocusedWindow().minimize());
+document.getElementById('maximize').addEventListener('click', toggleMaximize);
+document.getElementById('close').addEventListener('click', () => BrowserWindow.getFocusedWindow().close());
+
+// Event listeners for modal controls
+createModpackButton.onclick = () => modal.style.display = "block";
+closeButton.onclick = closeModalAndResetForm;
+window.onclick = (event) => { if (event.target == modal) closeModalAndResetForm(); };
+
+// Event listeners for form controls
+modpackLogoButton.onclick = () => modpackLogoInput.click();
+modpackLogoInput.onchange = () => modpackLogoButton.textContent = modpackLogoInput.value ? 'File Chosen' : 'Choose File';
+
+// Event listener for form submission
+form.addEventListener('submit', handleFormSubmission);
+
+function switchView(activeButton, inactiveButton, activeView, inactiveView) {
+    if (!activeButton.classList.contains('active')) {
+        document.getElementById(activeView).style.display = 'grid';
+        document.getElementById(inactiveView).style.display = 'none';
+        activeButton.classList.add('active');
+        inactiveButton.classList.remove('active');
     }
-  });
-  
-  browseModpacksButton.addEventListener('click', function() {
-    if (!browseModpacksButton.classList.contains('active')) {
-      document.getElementById('my-modpacks').style.display = 'none';
-      document.getElementById('browse-modpacks').style.display = 'block';
-      browseModpacksButton.classList.add('active');
-      myModpacksButton.classList.remove('active');
-    }
-  });
-
-document.getElementById('minimize').addEventListener('click', () => {
-    console.log('Minimize button clicked')
-    BrowserWindow.getFocusedWindow().minimize()
-})
-
-document.getElementById('maximize').addEventListener('click', () => {
-    console.log('Maximize button clicked')
-    let window = BrowserWindow.getFocusedWindow()
-    if (window.isMaximized()) {
-        window.unmaximize()
-    } else {
-        window.maximize()
-    }
-})
-
-document.getElementById('close').addEventListener('click', () => {
-    console.log('Close button clicked')
-    BrowserWindow.getFocusedWindow().close()
-})
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("create-modpack-button");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// Get the form
-var form = document.getElementById('modpackForm');
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-  form.reset(); // Reset the form
-  document.getElementById('modpackLogoButton').textContent = 'Choose File'; // Reset the button text
+function toggleMaximize() {
+    let window = BrowserWindow.getFocusedWindow();
+    window.isMaximized() ? window.unmaximize() : window.maximize();
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+function closeModalAndResetForm() {
     modal.style.display = "none";
-    form.reset(); // Reset the form
-    document.getElementById('modpackLogoButton').textContent = 'Choose File'; // Reset the button text
-  }
+    form.reset();
+    modpackLogoButton.textContent = 'Choose File';
 }
 
-// Handle form submission
-document.getElementById('modpackForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  // Here you can handle the form submission. For now, we'll just log the input values.
-  console.log('Modpack Name: ' + document.getElementById('modpackName').value);
-  console.log('Modpack Logo: ' + document.getElementById('modpackLogo').value);
-});
-
-// Get the custom button and the file input
-var button = document.getElementById('modpackLogoButton');
-var fileInput = document.getElementById('modpackLogo');
-
-// When the user clicks the button, trigger the file input click
-button.onclick = function() {
-  fileInput.click();
+function handleFormSubmission(event) {
+    event.preventDefault();
+    createModpackTile();
+    closeModalAndResetForm();
 }
 
-// When a file is selected, update the button text
-fileInput.onchange = function() {
-  if (fileInput.value) {
-    button.textContent = 'File Chosen';
-  } else {
-    button.textContent = 'Choose File';
-  }
+function createModpackTile() {
+    var modpackName = document.getElementById('modpackName').value;
+    var modpackLogo = document.getElementById('modpackLogo').files[0];
+
+    var modpackTile = document.createElement('div');
+    modpackTile.className = 'modpack-tile';
+
+    var logoElement = document.createElement('img');
+    logoElement.src = URL.createObjectURL(modpackLogo);
+    modpackTile.appendChild(logoElement);
+
+    var nameElement = document.createElement('h2');
+    nameElement.textContent = modpackName;
+    modpackTile.appendChild(nameElement);
+
+    var authorElement = document.createElement('p');
+    authorElement.textContent = "Author:"
+    modpackTile.appendChild(authorElement);
+
+    document.getElementById('my-modpacks').appendChild(modpackTile);
 }
-
-document.getElementById('modpackForm').addEventListener('submit', function(event) {
-  // Prevent the form from submitting normally
-  event.preventDefault();
-
-  // Get the modpack name and logo from the form
-  var modpackName = document.getElementById('modpackName').value;
-  var modpackLogo = document.getElementById('modpackLogo').files[0];
-
-  // Create a new tile for the modpack
-  var modpackTile = document.createElement('div');
-  modpackTile.className = 'modpack-tile';
-
-  // Add the modpack name and logo to the tile
-  var nameElement = document.createElement('h2');
-  nameElement.textContent = modpackName;
-  modpackTile.appendChild(nameElement);
-
-  var logoElement = document.createElement('img');
-  logoElement.src = URL.createObjectURL(modpackLogo);
-  modpackTile.appendChild(logoElement);
-
-  // Add the tile to the modpacks container
-  document.getElementById('my-modpacks').appendChild(modpackTile);
-});
